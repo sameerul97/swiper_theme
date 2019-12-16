264a131edc9f41ae022bebc7fbc0cc9e73d8334b
Sameer Sahabudeen <sameersahabudeen@wks57408.bauer-uk.bauermedia.group>
2019-12-16 11:29:15 +0000

    test2

--- js/parent.js
+++ js/parent.js
@@ -1,1 +1,17 @@
 console.log("Running from Parent.JS")
+
+// Set parent body { overflow-y: hidden; }
+// Set iframe height { height: calc(100vh - 140px); }
+var heightToMinus = $(".navbar").outerHeight() + $(".play-bar").outerHeight() - 2;
+$("#csFrame").css({
+    "height": "calc(100vh - " + heightToMinus + "px )"
+})
+document.getElementById("csFrame").setAttribute("scrolling", "yes")
+$("body").css({
+    "overflow-y": "hidden"
+})
+document.addEventListener("DOMContentLoaded", function(event) {
+    document.getElementById('csFrame').src += '';
+
+
+});
